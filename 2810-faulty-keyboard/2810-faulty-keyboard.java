0adec78788b5c93098602c451f63696fc7ef7c1b
Zholdoshbek <101273431+1Zholdoshbek@users.noreply.github.com>
2023-08-06 19:20:16 +0600

    Time: 12 ms (100.00%), Space: 44.4 MB (50.00%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.java
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -9,16 +9,16 @@ class Solution {
         String ans="";
         String p="";
         for(int i=0;i<s.length();i++){
-            ans+=s.charAt(i);
+            p+=s.charAt(i);
             if(s.charAt(i)=='i'){
-                ans=reverseString(ans);
+                p=reverseString(p);
             }
         }
-        for(int i=0;i<ans.length();i++){
-            if(ans.charAt(i)=='i')
+        for(int i=0;i<p.length();i++){
+            if(p.charAt(i)=='i')
                 continue;
-            p+=ans.charAt(i);
+            ans+=p.charAt(i);
         }
-        return p;
+        return ans;
     }
 }
