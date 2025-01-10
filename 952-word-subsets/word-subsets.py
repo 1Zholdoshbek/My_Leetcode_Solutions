1bf8b9eca53a707594192d5fdec6a71e5c2ba94c
Zholdoshbek <101273431+1Zholdoshbek@users.noreply.github.com>
2025-01-10 21:00:38 +0600

    Time: 403 ms (55.38%) | Memory: 21.3 MB (21.91%) - LeetSync

---
+++ 952-word-subsets/word-subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+
+
+        max_freq = Counter()
+        for word in words2:
+            word_freq = Counter(word)
+            for char, freq in word_freq.items():
+                max_freq[char] = max(max_freq[char], freq)
+        
+       
+        ans = []
+        for word in words1:
+            word_freq = Counter(word)
+            if all(word_freq[char] >= max_freq[char] for char in max_freq):
+                ans.append(word)
+        
+        return ans
+
+                
+        
