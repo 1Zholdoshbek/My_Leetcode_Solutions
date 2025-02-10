bf825ed37fc22e45a9a183688a5c6c2f20d0a5b8
Zholdoshbek <101273431+1Zholdoshbek@users.noreply.github.com>
2025-02-10 19:44:04 +0600

    Time: 0 ms (100.00%) | Memory: 8.4 MB (87.14%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        
+        string ans;
+
+        for(const char c:s){
+            if(isdigit(c)){
+                ans.pop_back();
+            }
+            else{
+                ans+=c;
+            }
+        }
+        return ans;
+    }
+};
